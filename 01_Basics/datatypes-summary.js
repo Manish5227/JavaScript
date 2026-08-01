b7a353c9a7437606792049a85c0f78601dc524ee
Manish Kumar Yadav <manishyadav11155@gmail.com>
2026-08-01 14:00:30 +0000

    datatypes summary

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,21 @@
+// Primitive dataTypes
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Reference (Non-Primitive)
+// Array, Objects, Functions
+
+// JavaScript (JS) is a dynamically typed language
+// NO need to declare the data type of a variable
+
+const heros = ["Shaktiman", "Doga", "Nagraj"];
+
+let myObj = {
+    name : "Manish",
+    age : 21,
+}
+
+let myFunction = function() {
+    console.log("Hello World")
+}
+
+// Non Primitive datatypes are object
