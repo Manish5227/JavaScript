04fb918cab1c39012ca565fc71b957ccbfa6cf6a
Manish Kumar Yadav <manishyadav11155@gmail.com>
2026-08-01 07:43:31 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3+3) we are using node.js , NOT browser
+
+console.log(9+1)
+
+console.log("Radha")
+
+// number => 2 to power 53 - 1
+// bigint
+// string => ""
+// boolean => True/False
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+//object
+
+let name = "Manish"
+let age = 21
+let isLoggedIn = false
+let state;
+
+console.log(name,typeof"Manish");
+
+console.log(typeof age);
+console.log(typeof state);
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
