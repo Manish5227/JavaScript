243ad4ceba41f8b3fca8fc1bf2ce84c5cb1581d5
Manish Kumar Yadav <manishyadav11155@gmail.com>
2026-08-01 10:44:54 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+let score = 77
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log (valueInNumber);
+
+// "77" => 77
+// "77abc" => NaN
+// true => 1 , false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+// true => 1 , false => 0
+// "" => false
+// "manish" => true
+
+let x = 5
+let strx = String(x)
+console.log(typeof strx)
+console.log(x)
+
