23f8337147c553813786c0460ff3d9e13d11877b
Manish Kumar Yadav <manishyadav11155@gmail.com>
2026-08-03 11:22:54 +0000

    arrays

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,37 @@
+const myArr = [4,6,8,9,11,33]
+console.log(myArr);
+
+const myHero = ["Hanuman","Shaktiman"]
+console.log(myHero);
+
+const myArr2 = new Array(9,8,7,6,5)
+console.log(myArr2);
+console.log(myArr2[1]);
+
+// Arrays Methods
+
+myArr.push(44)
+myArr.push(55)
+myArr.push(66)
+myArr.pop
+myArr.unshift(1)
+myArr.shift
+console.log(myArr);
+console.log(myArr.includes(30));
+console.log(myArr.indexOf(77));
+
+const newArr = myArr.join()
+console.log(myArr);
+console.log(newArr);
+
+// Slice and Splice
+
+const arr = [1,2,3,4,5]
+console.log("A", arr);
+
+const myn1 = arr.slice(1,4)
+console.log(myn1);
+
+const myn2 = arr.splice(1,4);
+console.log("B", arr);
+console.log(myn2);
