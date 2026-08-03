a578b37766e23d1740f58fe10a1f121825a23aba
Manish Kumar Yadav <manishyadav11155@gmail.com>
2026-08-03 11:56:59 +0000

    done

--- 02_Basics/02_arrays.js
+++ 02_Basics/02_arrays.js
@@ -1,20 +1,20 @@
 const marvel_heros = ["thor", "ironman", "spiderman"]
 const dc_heros = ["batman", "superman", "flash"]
 
-// marvel_heros.push(dc_heros);
-// console.log(marvel_heros);
-// console.log(marvel_heros[3][1]);
+marvel_heros.push(dc_heros);
+console.log(marvel_heros);
+console.log(marvel_heros[3][1]);
 
-// const allHeros = marvel_heros.concat(dc_heros)
-// console.log(allHeros);
+const allHeros = marvel_heros.concat(dc_heros)
+console.log(allHeros);
 
-// const allHeros1 = [...marvel_heros, ...dc_heros]
-// console.log(allHeros1);
+const allHeros1 = [...marvel_heros, ...dc_heros]
+console.log(allHeros1);
 
-// const other_arr = [1,2,3,4,[5,6],7,[8,9,[10,11]]]
-// console.log(other_arr);
-// const another_arr = other_arr.flat(Infinity)
-// console.log(another_arr);
+const other_arr = [1,2,3,4,[5,6],7,[8,9,[10,11]]]
+console.log(other_arr);
+const another_arr = other_arr.flat(Infinity)
+console.log(another_arr);
 
 console.log(Array.isArray("Manish"));
 console.log(Array.from("Manish"));
